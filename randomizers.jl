1b19e0434fe3c480acf956947c7d00747a16f605
gxyau <GuoXian.Yau@gmail.com>
2019-09-04 19:07:04 +0200

    Temporary commit, still trying to fix the case preserving ratio

--- randomizers.jl
+++ randomizers.jl
@@ -1,56 +1,94 @@
 #=========================================================================
+    This file should contain any functions that uses randomness.
+    For instance, random permutation to perform cross validation
+    and random permutation for Wilcoxon rank tests.
+=========================================================================#
+
+# Packages, using StatsBase because sample allows replacement
+# while rand from Random does not replacement
+using Random, StatsBase, IterTools
+
+#=========================================================================
+    The function slice_index takes in an array index, positive integer
+    nsubindex, and split index into subindex sets with subindex length
+    as equal as possible.
+    - index::Array{Int, 1} is an array to be split into kfold subarrays.
+      Ignores step if length(index) <= k
+    - nsubindex::Int64 is number of subarrays to split into
+=========================================================================#
+function slice_index(index::Array{Int, 1}, nsubindex::Int64)
+    subindex     = Dict{ Int64, Array{Int64, 1} }() # Initialise output
+    len          = length(index) # Length of index
+    intervalsize = Int64(floor(len//nsubindex)) # approximate step size
+    rest         = len % nsubindex # number of elements not distributed
+    nextitem     = 1 # current first element in index without class
+    classsizes   = vcat( repeat([intervalsize + 1], rest),
+                    repeat([intervalsize], nsubindex - rest) )
+
+    # Slicing the index into subindices
+    for i in 1:nsubindex
+        # Define (k,v) pair for subindex
+        subindex[i] = index[ nextitem:(nextitem + classsizes[i] - 1) ]
+        # Updating pointer
+        nextitem += classsizes[i]
+    end
+
+    return (classes = subindex, sizes = classsizes)
+end
+
+#=========================================================================
     randomise_index takes an array of indices and returns an
     three arrays of indices for training, testing, and tuning.
     -   kfold is the number of groups the indices to be split into.
         Default value is 10.
     -   response is the variable correspond to each index. Defaults to
-        empty array. If ratio is set to true, an array of equal length
-        to index should be provided
-    -   ratio is a flag on whether or not the ratio of the response
-        should be taken into account. Defaults to false. If set to
-        true, then response cannot be empty.
+        empty array. If provided the index set will preserve the ratio
+        as accurate as possible.
+    TODO/IDEA: Seed?
 =========================================================================#
-# Packages, using StatsBase because sample allows replacement
-# while rand from Random does not replacement
-using StatsBase
+function randomize_index(index::Array{Int64, 1}, kfold = 10, response::Array{Int64, 1} = Array{Int64, 1}())
+    len       = length(index) # Length of index
+    perm      = sample(1:len, len, replace = false) # Random permutation
+    randind   = index[perm] # Randomizing indices, indices may not be continuous
 
-#=
-    TODO: This is wasting resources, should return k groups instead
-    IDEA: Dynamically name the groups ind1, ..., indk and return (ind1,..., indk)
-=#
-function randomise_index(index::Array{Int64, 1}, kfold = 10, response = [], ratio = false)
-    # Initialising output indices
-    train, test, tune = Array{Int64, 1}(), Array{Int64, 1}(), Array{Int64, 1}()
-
-    # Sampling
-    len = length(index) # Length of index
-    if !(ratio)
-        # ratio = false
-        stepsize = Int64(floor(len//k)) # len//k is the fraction
-        randind  = sample(index, len, replace = false)
-        test     = randind[1:stepsize]
-        tune     = randind[(stepsize+1) : (2*stepsize)]
-        train    = randind[(2*stepsize+1) : len]
-    else if response = []
-        # ratio = true, length(response) = 0
-        throw("response::Any cannot be empty when ratio = true")
-    else if len != length(response)
-        # ratio = true, length(index) != length(response)
+    if len <= kfold
+        indexsets = Dict{ Int64, Array{Int64, 1} }(
+            key => [ randind[key] ]
+            for key in 1:len
+        )
+    elseif isempty(response) # Ignore ratio of the response
+        indexsets = slice_index(randind, kfold).classes
+    elseif len != length(response)
+        # length(index) != length(response)
         throw("Length of index::Int64 and response::Any are different")
     else
-        # ratio = true, length(index) = length(response)
-        uniqueresp = unique(response) # unique elements in resposne
-        for u in uniqueresp
+        # length(index) = length(response)
+        uresp     = unique(response) # unique elements in resposne
+        randresp  = response[perm]   # Randomizing response with the same randomness of randindsubsetsubset
+        currsizes = repeat([0], kfold) # Size
+
+        # Initialising output variable
+        indexsets   = Dict{ Int, Array{Int,1}}(
+            key => Array{Int64, 1}() for key in 1:kfold
+        )
+
+        for u in uresp
+            # TODO Need a start index in for u in uresp, otherwise the resulting split may be very uneven
             # Dividing by each unique element in response
-            subindex   = index[response .== u]
-            sublen     = length(subindex)
-            stepsizeu  = Int64(floor(sublen//k))
-            randindu   = sample(subindex, sublen, replace = false)
-            test       = vcat(test, randindu[1:stepsizeu])
-            tune       = vcat(tune, randindu[(stepsizeu+1) : (2*stepsizeu)])
-            train      = vcat(train, randindu[(2*stepsizeu+1) : sublen])
+            leastind  = findmin(currsizes)[2] # Next array to concatenate
+            randsubindex          = randind[randresp .== u]
+            subslice, classsizes  = slice_index(randsubindex, kfold)
+            println(leastind)
+            for i in leastind:(leastind+kfold)
+                key            = (i % 10 == 0) ? 10 : (i % 10)
+                indexsets[key] = vcat(indexsets[key], subslice[key])
+            end
+            println(indexsets)
+
+            # Updating classsizes and leastind
+            currsizes += classsizes
         end
     end
 
-    return (train = train, tune = tune, test = test)
+    return indexsets
 end
